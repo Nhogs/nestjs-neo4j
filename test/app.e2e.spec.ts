a1d837876c41fc4033f43a49645f241cff967ab5
Popotojs <ciminera_frederic@hotmail.com>
2022-01-04 16:12:42 +0100

    move docker compose data in .neo4j folder try health check in docker compose add connectivity check

---
+++ test/app.e2e.spec.ts
@@ -0,0 +1,25 @@
+import neo4j, { Driver } from 'neo4j-driver';
+
+describe('App (e2e)', () => {
+  let driver: Driver;
+
+  beforeAll(async () => {
+    driver = neo4j.driver(
+      `neo4j://localhost:7687`,
+      neo4j.auth.basic('neo4j', 'test'),
+    );
+  });
+
+  afterAll(async () => {
+    await driver.close();
+  });
+
+  it('should verify connectivity', async () => {
+    expect(await driver.verifyConnectivity()).toMatchInlineSnapshot(`
+      Object {
+        "address": "localhost:7687",
+        "version": "Neo4j/4.4.2",
+      }
+    `);
+  });
+});
